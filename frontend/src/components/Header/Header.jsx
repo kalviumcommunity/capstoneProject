c4146cdd915892cf5dd0813d133d1c79c3c78cd0
mahanandhan <mahanandhanmanish@gmail.com>
2025-04-17 09:59:54 +0530

    Forntendinit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food</h2>
+        <p>Choose from a diverse menu featuring a delicious selection of dishes and enjoy your food with rating and choosing us</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
